faee2a9e5f4b57dac403e94b908d876eef48c146
Duane Newman <Duane Newman>
2017-03-19 09:27:35 -0500

    Add project files.

---
+++ GospelPark.Membership/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GospelPark.Membership.MvcApplication" Language="C#" %>
---
+++ GospelPark.Membership/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GospelPark.Membership.Startup))]
+namespace GospelPark.Membership
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
